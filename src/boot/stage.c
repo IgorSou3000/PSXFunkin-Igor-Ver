5a462208df75c93cbf7d8efdc46c23ca09ad1858
IgorSou3000 <90409597+IgorSou3000@users.noreply.github.com>
2022-03-25 14:37:22 -0300

    Fixed border in downscroll

--- src/boot/stage.c
+++ src/boot/stage.c
@@ -1700,8 +1700,10 @@ void Stage_Tick(void)
 				RECT health_back = {33, 210, 256, 4};
 				RECT health_border = {32, 209, 258, 6};
 			    if (stage.downscroll)
-					health_fill.y = health_border.y = health_back.y = 20;
-
+                            {
+			      health_fill.y = health_back.y = 20;
+                              health_border.y = 19;
+                             }
 		        //draw healthbar
 				Gfx_DrawRect(&health_fill, baro_r, baro_g, baro_b);
 				Gfx_DrawRect(&health_back, barp_r, barp_g, barp_b);
